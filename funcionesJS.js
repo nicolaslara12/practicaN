345dfdc40bacc5e897c16875d4e71e3e9690bd26
Nicolas Rivera <nriveralara21@gmail.com>
2022-04-27 21:02:52 -0500

    modific 1

--- funcionesJS.js
+++ funcionesJS.js
@@ -1,2 +1,29 @@
+
+        
 let nota1, nota2, nota3;
 let dato1, dato2, dato3;
+
+dato1 = window.prompt("primer numero" ,"0");
+nota1 = parseInt(dato1);
+
+dato2 = window.prompt("segundo numero" , "0");
+nota2 = parseInt(dato2);
+
+dato3 = window.prompt("primer numero" , "0");
+nota3 = parseInt(dato1);
+
+media = (nota1 + nota2 + nota3)/3;
+
+if (media >5 )
+
+{
+resultado = "suspendido";
+
+}
+else
+{
+ resultado = "aprobado";
+}
+
+document.write(`<h3>la noata final es ${resultado}</h3`);
+   
